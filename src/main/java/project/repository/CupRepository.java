3778c7d5a17ec8b7018c491a20d4ab9b15ce1de7
Peter Nemiarouski <nemiarouskipetr@gmail.com>
2021-06-28 01:45:06 +0300

    Update file validation.

--- src/main/java/project/repository/CupRepository.java
+++ src/main/java/project/repository/CupRepository.java
@@ -7,22 +7,47 @@ import java.io.File;
 import java.io.IOException;
 
 public class CupRepository {
+    File file = new File("src/main/resources/cup.json");
 
     public void saveTo(Cup cup) {
+
+        if (!file.exists()) {
+            try {
+                new File(file.getParent()).mkdir();
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            objectMapper.writeValue(new File("src/main/resources/cup.json"), cup);
+            objectMapper.writeValue(file, cup);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public Cup downloadFrom() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            return objectMapper.readValue(new File("src/main/resources/cup.json"), Cup.class);
-        } catch (IOException e) {
-            e.printStackTrace();
+
+        if (!file.exists()) {
+            try {
+                new File(file.getParent()).mkdir();
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (file.length() > 0) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                return objectMapper.readValue(file, Cup.class);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("File is empty. Nothing to download.");
         }
         return null;
     }
