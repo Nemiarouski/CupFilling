c5f0e9854bc51c64e1c394cf4e9e4fc6482767c1
Petr Nemerovsky <petr.nemerovsky@intexsoft.by>
2021-07-07 10:14:56 +0300

    Fix path to save json file.

--- src/main/java/com/intexsoft/testproject/repository/CupRepository.java
+++ src/main/java/com/intexsoft/testproject/repository/CupRepository.java
@@ -8,7 +8,8 @@ import java.io.IOException;
 import java.util.List;
 
 public class CupRepository {
-    private final File FILE = new File("/home/INTEXSOFT/petr.nemerovsky/Documents/JavaProjects/CupFilling/src/main/resources/cup.json");
+    private final static String PATH = "cup.json";
+    private final File file = new File(PATH);
     private final List<String> cupTypes = List.of("Cylinder", "Parallelepiped");
     private Cup cup;
 
@@ -26,13 +27,13 @@ public class CupRepository {
 
     public void saveTo() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.writeValue(FILE, cup);
+        objectMapper.writeValue(file, cup);
     }
 
     public void downloadFrom() throws IOException {
-        if (FILE.exists() && FILE.length() > 0) {
+        if (file.exists() && file.length() > 0) {
             ObjectMapper objectMapper = new ObjectMapper();
-            cup = objectMapper.readValue(FILE, Cup.class);
+            cup = objectMapper.readValue(file, Cup.class);
         } else {
             System.out.println("Some problems");
         }
