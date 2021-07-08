9f9295bcf46a1c927e161fc74bf6eaf6eeb1ea86
Petr Nemerovsky <petr.nemerovsky@intexsoft.by>
2021-07-08 17:57:32 +0300

    Create new tests.

--- src/test/java/com/intexsoft/testproject/service/CupServiceTest.java
+++ src/test/java/com/intexsoft/testproject/service/CupServiceTest.java
@@ -17,31 +17,22 @@ class CupServiceTest {
     }
 
     @Test
-    void deleteLiquid() {
+    void deleteMoreThanHave() {
         CupService cupService = new CupService();
         cupService.createCup(FactoryType.CYLINDER, 10, 10);
-        System.out.println("Capacity: " + cupService.getCup().getCapacity());
-        cupService.getCup().getLiquid().forEach(System.out::println);
-        System.out.println();
-        assertEquals(45, cupService.getCup().getCapacity() - cupService.usedCapacity(cupService.getCup().getLiquid()));
-        cupService.deleteLiquid(55.0);
-        cupService.getCup().getLiquid().forEach(System.out::println);
-        assertEquals(100, cupService.getCup().getCapacity() - cupService.usedCapacity(cupService.getCup().getLiquid()));
+        cupService.addLiquid(LiquidType.CREAM, 150.0);
+        cupService.addLiquid(LiquidType.OIL, 150.0);
+        cupService.deleteLiquid(500.0);
+        assertEquals(0, cupService.usedCapacity(cupService.getCup().getLiquid()));
     }
 
     @Test
-    void changeCup() {
+    void changeCupToLessSize() {
         CupService cupService = new CupService();
-        cupService.getCup().getLiquid().forEach(System.out::println);
+        cupService.createCup(FactoryType.CYLINDER, 10, 10);
+        cupService.addLiquid(LiquidType.CREAM, 150.0);
+        cupService.addLiquid(LiquidType.OIL, 150.0);
         cupService.changeCup(cupService.getCup(), FactoryType.PARALLELEPIPED, 3, 7);
-        System.out.println();
-        cupService.getCup().getLiquid().forEach(System.out::println);
-    }
-
-    @Test
-    void usedCapacity() {
-        CupService cupService = new CupService();
-        cupService.createCup(FactoryType.CYLINDER, 10, 10);
-        assertEquals(75, cupService.getCup().findCapacity() - cupService.usedCapacity(cupService.getCup().getLiquid()));
+        assertEquals(63, cupService.usedCapacity(cupService.getCup().getLiquid()));
     }
 }
